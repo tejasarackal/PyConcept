f540b6e23efca4f3661e2152217dadadc24fbfdb
Tejas Arackal <tva6@case.edu>
2019-12-17 23:52:16 -0500

    added base class AverageCalculator by inheriting the AbstractBaseClass(ABC) module. The astract methods has_next() and next_item() needs to be implemented by derived classes as these are the blueprints for the template function average() that has the algorithm to calculate average. Created derived implementations FileAverageCalculator and MemoryAverageCalculator, with the former using a file as input while latter using its memory list object as input. Both implement the has_next() and next_item() methods for calculatng the respective averages.

--- design-patterns/template-method.py
+++ design-patterns/template-method.py
@@ -9,7 +9,7 @@ class AverageCalculator(ABC):
             while self.has_next():
                 total += self.next_item()
                 items += 1
-            else:
+            if items == 0:
                 raise RuntimeError("No items found for calculating average")
             return total/items
         finally:
@@ -25,3 +25,50 @@ class AverageCalculator(ABC):
 
     def dispose(self):
         pass
+
+
+class FileAverageCalculator(AverageCalculator):
+
+    def __init__(self, file):
+        self.file = file
+        self.last_line = self.file.readline()
+
+    def has_next(self):
+        return self.last_line != ""
+
+    def next_item(self):
+        next_ = float(self.last_line)
+        self.last_line = self.file.readline()
+        return next_
+
+    def dispose(self):
+        self.file.close()
+
+
+class MemoryAverageCalculator(AverageCalculator):
+
+    def __init__(self, list_: list()):
+        self.list = iter(list_)
+
+    def has_next(self):
+        return self.list.__length_hint__() > 0
+
+    def next_item(self):
+        return next(self.list)
+
+    #def average(self):
+    #    if len(self.list) == 0:
+    #        raise RuntimeError("No items found for calculating average")
+    #    try:
+    #        return sum(self.list) / len(self.list)
+    #    finally:
+    #        self.dispose()
+
+
+if __name__ == '__main__':
+    with open('numbers.txt') as file:
+        f = FileAverageCalculator(file)
+        print(f"Average for numbers in file is {f.average()}")
+
+    m = MemoryAverageCalculator([1,2,3,4,5,6,7])
+    print(f"Average of numbers in a list memory is {m.average()}")
